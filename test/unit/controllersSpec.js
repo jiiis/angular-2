b192ae08b5a9f17ca5df139136a59368eff3e116
Hao Chang <yngwie.chang@gmail.com>
2015-09-06 22:27:18 +1000

    Tried Jasmine unit test on a controller.

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('erNiuNiu controllers', function() {
+    describe('PhoneListCtrl', function() {
+        beforeEach(module('erNiuNiu'));
+
+        it('should should create model "phones" with 3 phones', inject(function($controller) {
+            var scope = {},
+                ctrl = $controller('PhoneListCtrl', {
+                    $scope: scope
+                });
+
+            expect(scope.phones.length).toBe(3);
+        }))
+    });
+});
