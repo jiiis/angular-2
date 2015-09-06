28d4cbeed8830729c1856dd0e618f8a8d39def21
Hao Chang <yngwie.chang@gmail.com>
2015-09-06 22:26:00 +1000

    Installed package "angular-mocks" via Bower and loaded "angular-mocks.js" to Karma.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
         frameworks: ['jasmine'],
         files: [
             'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
             'app/js/**/*.js',
             'test/unit/**/*.js'
         ],
@@ -15,6 +16,10 @@ module.exports = function(config) {
         logLevel: config.LOG_DEBUG,
         autoWatch: true,
         browsers: ['Chrome'],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+        ],
         singleRun: false
     });
 };
