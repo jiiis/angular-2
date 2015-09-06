3837b0742ed94443fe55ea904022c8481d3931b3
Hao Chang <yngwie.chang@gmail.com>
2015-09-06 21:53:07 +1000

    Updated "basePath" and "files" entries in "test/karma.conf.js".

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,8 +1,12 @@
 module.exports = function(config) {
     config.set({
-        basePath: '',
+        basePath: '../',
         frameworks: ['jasmine'],
-        files: [],
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
         exclude: [],
         preprocessors: {},
         reporters: ['dots', 'progress'],
