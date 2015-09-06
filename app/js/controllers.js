9d64226959cf40550527633cc6d7f2aa3d80d03a
Hao Chang <yngwie.chang@gmail.com>
2015-09-06 20:25:51 +1000

    Tried directives ngApp, ngController and ngRepeat.

---
+++ app/js/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var erNiuNiu = angular.module('erNiuNiu', []);
+
+erNiuNiu.controller('PhoneListCtrl', function($scope) {
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            description: 'Fast just got faster with Nexus S.'
+        },
+        {
+            name: 'Motorola XOOM™ with Wi-Fi',
+            description: 'The Next, Next Generation tablet.'
+        },
+        {
+            name: 'MOTOROLA XOOM™',
+            description: 'The Next, Next Generation tablet.'
+        }
+    ];
+});
