91e84ba45b88074c2b14261bf101f334b7b8b2d8
Hao Chang <yngwie.chang@gmail.com>
2015-09-19 21:26:20 +1000

    Added e2e tests for the "sort" functionality.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,26 +6,51 @@ describe('erNiuNiu', function() {
             browser.get('app/index.html');
         });
 
-        var phoneList = element.all(by.repeater('phone in phones')),
-            query = element(by.model('query'));
+        var $phones = element.all(by.repeater('phone in phones')),
+            $query = element(by.model('query'));
 
         it('should filter the phone list as the user types into the search box', function() {
-            expect(phoneList.count()).toBe(3);
+            expect($phones.count()).toBe(3);
 
-            query.sendKeys('nexus');
-            expect(phoneList.count()).toBe(1);
+            $query.sendKeys('nexus');
+            expect($phones.count()).toBe(1);
 
-            query.clear();
-            query.sendKeys('motorola');
-            expect(phoneList.count()).toBe(2);
+            $query.clear();
+            $query.sendKeys('motorola');
+            expect($phones.count()).toBe(2);
         });
 
         it('should display the current filter value as the page title', function() {
-            query.clear();
+            $query.clear();
             expect(browser.getTitle()).toMatch(/^erNiuNiu:\s*$/);
 
-            query.sendKeys('nexus');
+            $query.sendKeys('nexus');
             expect(browser.getTitle()).toMatch(/^erNiuNiu: nexus$/);
+        });
+
+        it('should sort the phone list as the user chooses an orderBy option', function() {
+            var $phoneNameColumns = element.all(by.repeater('phone in phones').column('phone.name')),
+                $orderByName = element(by.model('order')).element(by.css('option[value=name]')),
+                getPhoneNames = function() {
+                    return $phoneNameColumns.map(function($e) {
+                        return $e.getText();
+                    });
+                };
+
+            $query.clear();
+            $query.sendKeys('tablet');
+
+            expect(getPhoneNames()).toEqual([
+                'Motorola XOOM\u2122 with Wi-Fi',
+                'Motorola XOOM\u2122'
+            ]);
+
+            $orderByName.click();
+
+            expect(getPhoneNames()).toEqual([
+                'Motorola XOOM\u2122',
+                'Motorola XOOM™ with Wi-Fi'
+            ]);
         });
     });
 });
