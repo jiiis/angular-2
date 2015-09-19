c7cc71228d5a5de356d1aa9872ad1ba0334b49fd
Hao Chang <yngwie.chang@gmail.com>
2015-09-19 20:13:24 +1000

    Added "sort" functionality and tuned the layout using Bootstrap.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,15 +6,20 @@ erNiuNiu.controller('PhoneListCtrl', function($scope) {
     $scope.phones = [
         {
             name: 'Nexus S',
-            description: 'Fast just got faster with Nexus S.'
+            description: 'Fast just got faster with Nexus S.',
+            age: 3
         },
         {
             name: 'Motorola XOOM™ with Wi-Fi',
-            description: 'The Next, Next Generation tablet.'
+            description: 'The Next, Next Generation tablet.',
+            age: 1
         },
         {
             name: 'Motorola XOOM™',
-            description: 'The Next, Next Generation tablet.'
+            description: 'The Next, Next Generation tablet.',
+            age: 2
         }
     ];
+
+    $scope.order = 'age';
 });
