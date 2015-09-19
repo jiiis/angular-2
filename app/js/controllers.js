e018eb213f013e851e63ec7771813d3bdeb0346f
Hao Chang <yngwie.chang@gmail.com>
2015-09-20 00:48:12 +1000

    Tried ".splice(0, 10)" to pre-process the http response by limiting the number of items.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@ var erNiuNiu = angular.module('erNiuNiu', []);
 
 erNiuNiu.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('model/phones.json').success(function(phones) {
-        $scope.phones = phones;
+        $scope.phones = phones.splice(0, 10);
     });
 
     $scope.order = 'age';
