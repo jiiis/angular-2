c56b68da1f01fae73eae26602ae6e58d176febf5
Hao Chang <yngwie.chang@gmail.com>
2015-09-20 00:08:24 +1000

    Added unit tests for XHR.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,18 +2,31 @@
 
 describe('erNiuNiu controllers', function() {
     describe('PhoneListCtrl', function() {
-        var ctrl, scope;
+        var ctrl, scope, $httpBackend;
 
         beforeEach(module('erNiuNiu'));
-        beforeEach(inject(function($controller) {
-            scope = {};
+
+        beforeEach(inject(function($controller, $rootScope, _$httpBackend_) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('model/phones.json').respond([
+                {name: 'Nexus S'},
+                {name: 'Motorola DROID'}
+            ]);
+
+            scope = $rootScope.$new();
             ctrl = $controller('PhoneListCtrl', {
                 $scope: scope
             });
         }));
 
-        it('should create model "phones" with 3 phones', function() {
-            expect(scope.phones.length).toBe(3);
+        it('should create model "phones" with 2 items fetched from XHR', function() {
+            expect(scope.phones).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.phones).toEqual([
+                {name: 'Nexus S'},
+                {name: 'Motorola DROID'}
+            ]);
         });
 
         it('should set the correct default value of order', function() {
