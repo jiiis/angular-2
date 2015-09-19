21e2a41dcf96b840ce613d8f593b21a234d70df9
Hao Chang <yngwie.chang@gmail.com>
2015-09-19 20:25:32 +1000

    Tuned units tests for controllers and added a test for scope.order of PhoneListCtrl.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,15 +2,22 @@
 
 describe('erNiuNiu controllers', function() {
     describe('PhoneListCtrl', function() {
+        var ctrl, scope;
+
         beforeEach(module('erNiuNiu'));
-
-        it('should create model "phones" with 3 phones', inject(function($controller) {
-            var scope = {},
-                ctrl = $controller('PhoneListCtrl', {
-                    $scope: scope
-                });
-
-            expect(scope.phones.length).toBe(3);
+        beforeEach(inject(function($controller) {
+            scope = {};
+            ctrl = $controller('PhoneListCtrl', {
+                $scope: scope
+            });
         }));
+
+        it('should create model "phones" with 3 phones', function() {
+            expect(scope.phones.length).toBe(3);
+        });
+
+        it('should set the correct default value of order', function() {
+            expect(scope.order).toBe('age');
+        });
     });
 });
