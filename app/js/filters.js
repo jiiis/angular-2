2dff771385efbbeaa95f8290cedb59441152a6ef
Hao Chang <yngwie.chang@gmail.com>
2015-10-04 23:19:46 +1100

    Added a custom filter for transforming a boolean value into a unicode charactor.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var erFilters = angular.module('erFilters', []);
+
+erFilters.filter('checkmark', function() {
+    return function(input) {
+        return input ? '\u2713' : '\u2718';
+    };
+});
