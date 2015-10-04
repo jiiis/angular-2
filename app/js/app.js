6e61494f783b121a65ff1bff3c6f8eb12e845ade
Hao Chang <yngwie.chang@gmail.com>
2015-10-04 17:32:09 +1100

    Used "$routeProvider" to configure routes.

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,14 @@ var erNiuNiu = angular.module('erNiuNiu', [
     'ngRoute',
     'erControllers'
 ]);
+
+erNiuNiu.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'PhoneListCtrl'
+        })
+        .otherwise({
+           redirectTo: '/phones'
+        });
+}]);
