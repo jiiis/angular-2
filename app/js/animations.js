40187c4b14e84690fda9b7514d754d0219dd5e98
Hao Chang <yngwie.chang@gmail.com>
2015-10-07 01:10:52 +1100

    Added JS animation to the image swapper in the "phone detail" view.

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,3 +1,52 @@
 'use strict';
 
 var erAnimations = angular.module('erAnimations', ['ngAnimate']);
+
+erAnimations.animation('.phone-detail-main-image', function() {
+    var animateUp = function(element, className, done) {
+        if (className !== 'active') {
+            return;
+        }
+
+        element.css({
+            position: 'absolute',
+            top: 300
+        });
+
+        element.animate({
+            top: 0
+        }, 500, done);
+
+        return function(cancel) {
+            if (cancel) {
+                element.stop();
+            }
+        };
+    };
+
+    var animateDown = function(element, className, done) {
+        if (className !== 'active') {
+            return;
+        }
+
+        element.css({
+            position: 'absolute',
+            top: 0
+        });
+
+        element.animate({
+            top: -300
+        }, 500, done);
+
+        return function(cancel) {
+            if (cancel) {
+                element.stop();
+            }
+        };
+    };
+
+    return {
+        addClass: animateUp,
+        removeClass: animateDown
+    };
+});
